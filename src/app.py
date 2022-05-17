d2fda690b07383c1892eb447b088089980f11ab8
Kankan <kankankalita223@gmail.comgit statusgit config --global user.email kankankalita223@gmail.com>
2022-05-17 21:57:23 +0530

    fixed the 'other optons'

--- src/app.py
+++ src/app.py
@@ -2,14 +2,22 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
-
-
-@app.route("/template")
-def template():
+@app.route("/home")
+def home():
     return render_template("index.html")
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/logout")
+def logout():
+    return render_template("logout.html")
+
+@app.route("/settings")
+def settings():
+    return render_template("settings.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
