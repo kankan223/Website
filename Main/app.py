0ff527b390d05f9690e9a3a3ecb133c0559e78a3
Kankan <kankankalita223@gmail.comgit statusgit config --global user.email kankankalita223@gmail.com>
2022-05-17 20:49:42 +0530

    added base.html

--- Main/app.py
+++ Main/app.py
@@ -3,12 +3,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def home():
     return "<p>Hello, World!</p>"
 
 
 @app.route("/template")
-def reender():
+def render():
     return render_template("index.html")
 
 if __name__ == "__main__":
