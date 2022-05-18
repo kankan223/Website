c44a4d15b59502e46cf9e404f0de8d179b17afe5
Kankan <kankankalita223@gmail.comgit statusgit config --global user.email kankankalita223@gmail.com>
2022-05-18 14:21:33 +0530

    added new  bootstrap

--- src/app.py
+++ src/app.py
@@ -4,6 +4,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
+    return render_template("new.html")
+
+@app.route("/index")
+def index():
     return render_template("index.html")
 
 
@@ -19,6 +23,10 @@ def logout():
 def settings():
     return render_template("settings.html")
 
+
+@app.route("/profile")
+def profile():
+    return render_template("profile.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
-    
