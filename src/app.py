ecb16c5c432b5c72350b2b828c873bbe491ed557
Kankan <kankankalita223@gmail.comgit statusgit config --global user.email kankankalita223@gmail.com>
2022-05-21 22:32:34 +0530

    fixed

---
+++ src/app.py
@@ -0,0 +1,69 @@
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+<<<<<<< HEAD
+import json
+import os
+=======
+
+>>>>>>> 2ad56202fd467cf4a987424fae20da5981df1baf
+
+
+
+with open('/templates/config.json', 'r') as c:
+    params = json.load(c)["params"]
+
+local_server = params['local_serer']    
+
+
+app = Flask(__name__)
+if(local_server):
+    app.config['SQLALCHEMY_DATABASE_URI'] = params['local_uri']
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = params['prod_uri']
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class website(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable=False)
+    email = db.Column(db.String, nullable=False)
+    password = db.Column(db.String, nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self) -> str:
+        return f"{self.sno} - {self.name}"
+
+
+@app.route("/")
+def home():
+    
+    #db.session.add(website)
+    #db.session.commit()
+    return render_template("index.html")
+
+@app.route("/index")
+def new():
+    return render_template("new.html")
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/logout")
+def logout():
+    return render_template("logout.html")
+
+@app.route("/settings")
+def settings():
+    return render_template("settings.html")
+
+
+@app.route("/profile")
+def profile():
+    return render_template("profile.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
